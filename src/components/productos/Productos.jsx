5e46e3ce03e13f073f6489d6ecf2f9f624db1c3b
edG5LATAN <136633393+edG5LATAN@users.noreply.github.com>
2025-06-22 15:55:55 -0600

    actualizacion de lista

--- src/components/productos/Productos.jsx
+++ src/components/productos/Productos.jsx
@@ -5,42 +5,55 @@ import { datos } from "../service/datos/Datos";
 import { data } from "react-router-dom";
 
 function Productos() {
+  const [info, setinfo] = useState(datos);
+  const [pro, setpro] = useState("");
 
-  const [info,setinfo]=useState(datos)
-
+  const buscarProductos = (texto) => {
+    const textoBusqueda = texto.toLowerCase();
+    const newArrary=datos.filter(
+        (producto) =>
+          producto.marca.toLowerCase().includes(textoBusqueda) ||
+          producto.nombre.toLowerCase().includes(textoBusqueda)
+      )
+    setinfo(newArrary)
+    console.log(info)
+    setpro("")
+  };
 
   return (
-    <div className="producto_contenedor" >
+    <div className="producto_contenedor">
       <div className="producto_buscador">
-        <form className="d-flex" role="search">
+        <div className="d-flex producto_form" role="search">
           <input
+            value={pro}
+            onChange={(e) => setpro(e.target.value)}
             class="form-control me-2"
             type="search"
             placeholder="Search"
             aria-label="Search"
           />
-          <button class="btn btn-outline-success" type="submit">
+          <button
+            onClick={()=>{
+              buscarProductos(pro)}
+
+            }
+            class="btn btn-outline-success"
+            type="submit"
+          >
             Search
           </button>
-        </form>
+        </div>
       </div>
       <div className="producto_card">
-        {
-          info==[]
-          ?
+        {info == [] ? (
+          <>cargando...</>
+        ) : (
           <>
-          cargando...
+            {info.map((res, index) => {
+              return <Cards key={index} producto={res} />;
+            })}
           </>
-          :
-          <>
-          {
-            info.map((res,index)=>{
-              return<Cards key={index} producto={res}/>
-            })
-          }
-          </>
-          
-        }
+        )}
       </div>
     </div>
   );
--- src/components/service/datos/Datos.js
+++ src/components/service/datos/Datos.js
@@ -39,8 +39,8 @@ export const datos = [
     id: 5,
     marca: "yummies",
     nombre:"con queso",
-    cantidad: "ranchitas",
-    tipo: "normal",
+    cantidad: "16 onz",
+    tipo: "ranchita",
     imagen: "https://media.larachycia.com/800x800/media/items/83060001.webp",
     precio: 15
   },
@@ -48,17 +48,17 @@ export const datos = [
     id: 6,
     marca: "yummies",
     nombre: "picante",
-    cantidad: "chicharron picante",
-    tipo: "normal",
+    cantidad: "16 onz",
+    tipo: "chicharron",
     imagen: "https://snacksyummies.com/wp-content/uploads/2020/11/Chicharron-Picositos.webp",
     precio: 15
   },
   {
     id: 7,
     marca: "yummies",
-    nombre: "normal",
-    cantidad: "chicharron nomal",
-    tipo: "normal",
+    nombre: "sin chile",
+    cantidad: "16 onz",
+    tipo: "chicharron",
     imagen: "https://lacolonia.vtexassets.com/arquivos/ids/249239-800-800?v=638689306391000000&width=800&height=800&aspect=true",
     precio: 15
   },
@@ -101,9 +101,9 @@ export const datos = [
   {
     id: 12,
     marca: "diana",
-    nombre: "maizchino",
+    nombre: "maiz chino",
     cantidad: "90g",
-    tipo: "normal",
+    tipo: "picante",
     imagen: "https://worldtradegoods.com/cdn/shop/files/001_934dda9c-1050-4e0f-9451-eccf3b6a328d.png?v=1733771928",
     precio: 9
   },
@@ -124,5 +124,150 @@ export const datos = [
     tipo: "normal",
     imagen: "https://delvalleie.com/wp-content/uploads/2021/01/PALITOS_ORIGINAL-3-800x800.png",
     precio: 9
+  },
+  {
+    id: 15,
+    marca: "sula",
+    nombre: "margarina",
+    cantidad: "caja",
+    tipo: "cremmargarinaa",
+    imagen: "https://sula.hn/wp-content/uploads/2020/04/0012_productos-sula-mantequilla-amarilla-400g.jpg",
+    precio: 43
+  },
+  {
+    id: 16,
+    marca: "banilla",
+    nombre: "spaghetti",
+    cantidad: "500g",
+    tipo: "normal",
+    imagen: "https://www.allservefoodservice.com/wp-content/uploads/2017/10/Spaghetti-Mediano-Barilla-25-500-gr-1.jpg",
+    precio: 15
+  },
+  {
+    id: 17,
+    marca: "maggi",
+    nombre: "cubito",
+    cantidad: "unidad",
+    tipo: "de pollo",
+    imagen: "https://walmarthn.vtexassets.com/arquivos/ids/382102/Cubito-De-Pollo-Maggi-Sazonador-Caja-100g-2-2857.jpg?v=638448825654230000",
+    precio: 5
+  },
+  {
+   id: 18,
+    marca: "maseca",
+    nombre: "maseca",
+    cantidad: "250g",
+    tipo: "de maiz",
+    imagen: "https://media.nidux.net/pull/600/800/13407/40542-product-67e203354a02a-020645.jpg",
+    precio: 35
+  },
+  {
+   id: 19,
+    marca: "aguazul",
+    nombre: "aguazul",
+    cantidad: "bolsita",
+    tipo: "agua",
+    imagen: "https://igtravellabel.wordpress.com/wp-content/uploads/2016/06/bolsa-de-agua.jpeg?w=640",
+    precio: 5
+  },
+  {
+   id: 20,
+    marca: "primavera",
+    nombre: "margarina",
+    cantidad: "unidad",
+    tipo: "margarina",
+    imagen: "https://chedrauimx.vtexassets.com/arquivos/ids/49204897/8719200450516_00.jpg?v=638845996593330000",
+    precio: 12 
+  },
+  {
+   id: 21,
+    marca: "tigo",
+    nombre: "super 3 dias",
+    cantidad: "3 dias",
+    tipo: "super recarga",
+    imagen: "https://lacoloniaqa.vteximg.com.br/arquivos/ids/170647-650-500/LA-COLONIA-PAGINA-WEB-1.jpg?v=638375585583800000",
+    precio: 38 
+  },
+  {
+   id: 22,
+    marca: "tigo",
+    nombre: "super 15 dias",
+    cantidad: "15 dias",
+    tipo: "super recarga",
+    imagen: "https://lacoloniaqa.vteximg.com.br/arquivos/ids/170647-650-500/LA-COLONIA-PAGINA-WEB-1.jpg?v=638375585583800000",
+    precio: 68 
+  },
+  {
+   id: 23,
+    marca: "claro",
+    nombre: "super 3 dias",
+    cantidad: "3 dias",
+    tipo: "super recarga",
+    imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsFRdiD5hZoQyylNmZ_w4RUJAbydc7fh7DpQ&s",
+    precio: 35 
+  },
+  {
+   id: 24,
+    marca: "claro",
+    nombre: "super 15 dias",
+    cantidad: "15 dias",
+    tipo: "super recarga",
+    imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsFRdiD5hZoQyylNmZ_w4RUJAbydc7fh7DpQ&s",
+    precio: 65 
+  },
+  {
+   id: 25,
+    marca: "cocacola",
+    nombre: "cocacola",
+    cantidad: "desechable",
+    tipo: "plastico",
+    imagen: "https://lacolonia.vtexassets.com/arquivos/ids/191396-800-800?v=637124493966270000&width=800&height=800&aspect=true",
+    precio: 28 
+  },
+  {
+   id: 26,
+    marca: "cocacola",
+    nombre: "cocacola",
+    cantidad: "2Litros",
+    tipo: "plastico",
+    imagen: "https://latiendatotal.com/1132-large_default/coca-cola-desechable-2-litros.jpg",
+    precio: 45 
+  },
+  {
+   id: 27,
+    marca: "cocacola",
+    nombre: "cocacola",
+    cantidad: "1.5 Litros",
+    tipo: "vidrio",
+    imagen: "https://labodega.com.pe/Adminbackend/fotos/producto14376.jpg",
+    precio: 35 
+  },
+  {
+   id: 28,
+    marca: "cocacola",
+    nombre: "cocacola",
+    cantidad: "500 mg",
+    tipo: "vidrio",
+    imagen: "https://target.scene7.com/is/image/Target/GUEST_57b5917a-43cb-4602-aa0b-757c48d50b2e?wid=800&hei=800&qlt=80&fmt=webp",
+    precio: 25 
+  },
+  {
+   id: 29,
+    marca: "cocacola",
+    nombre: "uva",
+    cantidad: "500 mg",
+    tipo: "desechable",
+    imagen: "https://acosa.com.hn/wp-content/uploads/2022/11/A54426.png",
+    precio: 25 
+  },
+  {
+   id: 30,
+    marca: "cocacola",
+    nombre: "uva",
+    cantidad: "2 litros",
+    tipo: "plastico",
+    imagen: "https://lacolonia.vtexassets.com/arquivos/ids/184756/360-Bebidas-y-Jugos-Refrescos-Refrescos-de-Sabores_784562014506_1.jpg?v=637117566868000000",
+    precio: 45 
   }
+
 ]
